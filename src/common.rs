99950442945fcfd0a9a80ce9ec9853136cc66fec
Vesa Kaihlavirta <vegai@iki.fi>
2016-05-27 17:34:41 +0300

    Move non model function away from model

--- src/common.rs
+++ src/common.rs
@@ -4,6 +4,8 @@ extern crate rand;
 
 use std::iter::repeat;
 use std::fs;
+use std::io;
+use std::fs::File;
 use self::crypto::salsa20::Salsa20;
 use self::crypto::symmetriccipher::SynchronousStreamCipher;
 use self::serialize::base64;
@@ -84,15 +86,15 @@ pub fn generate_salt() -> Vec<u8> {
 }
 
 pub fn load_or_create_key(filename: &str) -> Vec<u8> {
-    match Passwords::load_file(filename) {
+    match load_file(filename) {
         Ok(s) => s.from_base64().expect("Key base64 decoding failed"),
         Err(_) => {
             println!("Creating a new key in {}", filename);
             let mut rng = OsRng::new().expect("OsRng init failed");
             let new_key: Vec<u8> = rng.gen_iter::<u8>().take(KEY_LENGTH).collect();
             let key_base64 = new_key.to_base64(base64::STANDARD);
-            Passwords::save_data(&key_base64, filename);
-            Passwords::set_file_perms(filename, 0o400);
+            save_data(&key_base64, filename);
+            set_file_perms(filename, 0o400);
             new_key
         }
     }
@@ -104,6 +106,25 @@ pub fn create_data_dir(data_dir: &str) {
     Passwords::set_file_perms(&data_dir, 0o700);
 }
 
+pub fn load_file(path: &str) -> Result<String, io::Error> {
+    let mut f = try!(File::open(path));
+    let mut s = String::new();
+    try!(f.read_to_string(&mut s));
+    Ok(s)
+}
+
+pub fn save_data(data: &str, filename: &str) {
+    let mut f = File::create(filename).unwrap();
+    f.write_all(data.as_bytes()).expect("Data file write failed");
+    f.write_all(b"\n").expect("Newline write failed!?");
+    f.sync_all().expect("Sync failed");
+}
+
+pub fn set_file_perms(filename: &str, mode: u32) {
+    let mut perms = fs::metadata(filename).expect("Gettings perms failed").permissions();
+    perms.set_mode(mode);
+    fs::set_permissions(filename, perms).expect("Setting permission failed");
+}
 
 #[test]
 fn test_cut_password() {
